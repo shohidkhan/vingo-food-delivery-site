059284a643e8b2bb94002176a7ce3c01dc03d538
shahidullah <shohidkhan4361@gmail.com>
2026-07-12 11:59:46 +0600

    üserlocationUpdate logic implemented

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -14,3 +14,26 @@ export const getCurrentUser = async (req, res) => {
     res.status(500).json(`get current user error: ${error.message}`);
   }
 };
+
+export const userUpdateLocation = async (req, res) => {
+  try {
+    const { lat, lon } = req.body;
+
+    const userId = req.userId;
+
+    const user = await User.findByIdAndUpdate(userId, {
+      location: {
+        type: "Point",
+        coordinates: [lat, lon],
+      },
+    });
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json(`user update location error: ${error.message}`);
+  }
+};
--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,7 +1,11 @@
 import express from "express";
 import isAuth from "../middlewares/IsAuth.js";
-import { getCurrentUser } from "../controllers/user.controller.js";
+import {
+  getCurrentUser,
+  userUpdateLocation,
+} from "../controllers/user.controller.js";
 const userRouter = express.Router();
 
 userRouter.get("/current-user", isAuth, getCurrentUser);
+userRouter.put("/update-location", isAuth, userUpdateLocation);
 export default userRouter;
